2f394a9666ace13e7ebdcf86523f95c5695a7141
Meryem DRIDI <82763056+meryemdrd@users.noreply.github.com>
2021-07-29 12:48:30 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+  
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './component/NavBar';
+
+import Profil from './component/Profil';
+
+
+function App() {
+  return (
+    <div className="App">
+     <NavBar/>
+    <Profil/>
+    
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Profil.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Profil = () => {
+    return (
+        <div>
+            <h1 style={{color: "red"}}> CV </h1>
+            <p> Hello i'am meriem</p>
+          
+        </div>
+    )
+}
+
+export default Profil
